002cf9ddc4789ebbadc9806c2ee032f5ced02a00
sarthak3154 <sarthak.arora3154@gmail.com>
2019-03-09 15:49:07 -0500

    ai recycler move level 1 children
    get available pickable cards
    
    placed cards children states

--- main.py
+++ main.py
@@ -10,21 +10,22 @@ from MiniMax import *
 from AlphaBeta import *
 from Player import *
 from StateNode import *
-from State import *
+from State import State
 from random import randint
+
 # from tabulate import tabulate
 
-DOT = ['BLACK_DOT','WHITE_DOT']
-COLOR = ['RED_COLOR','WHITE_COLOR']
-DIRECTION = ['HORIZONTAL' , 'VERTICAL']
-ROTATIONS = [[DIRECTION[0],DOT[0],COLOR[0],DOT[1],COLOR[1]],
-             [DIRECTION[1],DOT[1],COLOR[1],DOT[0],COLOR[0]],
-             [DIRECTION[0],DOT[1],COLOR[1],DOT[0],COLOR[0]],
-             [DIRECTION[1],DOT[0],COLOR[0],DOT[1],COLOR[1]],
-             [DIRECTION[0],DOT[1],COLOR[0],DOT[0],COLOR[1]],
-             [DIRECTION[1],DOT[0],COLOR[1],DOT[1],COLOR[0]],
-             [DIRECTION[0],DOT[0],COLOR[1],DOT[1],COLOR[0]],
-             [DIRECTION[1],DOT[1],COLOR[0],DOT[0],COLOR[1]]]
+DOT = ['BLACK_DOT', 'WHITE_DOT']
+COLOR = ['RED_COLOR', 'WHITE_COLOR']
+DIRECTION = ['HORIZONTAL', 'VERTICAL']
+ROTATIONS = [[DIRECTION[0], DOT[0], COLOR[0], DOT[1], COLOR[1]],
+             [DIRECTION[1], DOT[1], COLOR[1], DOT[0], COLOR[0]],
+             [DIRECTION[0], DOT[1], COLOR[1], DOT[0], COLOR[0]],
+             [DIRECTION[1], DOT[0], COLOR[0], DOT[1], COLOR[1]],
+             [DIRECTION[0], DOT[1], COLOR[0], DOT[0], COLOR[1]],
+             [DIRECTION[1], DOT[0], COLOR[1], DOT[1], COLOR[0]],
+             [DIRECTION[0], DOT[0], COLOR[1], DOT[1], COLOR[0]],
+             [DIRECTION[1], DOT[1], COLOR[0], DOT[0], COLOR[1]]]
 
 print('\n\t\t:::: Double Board Game ::::')
 NUM_PLAYERS = 2
@@ -49,12 +50,12 @@ def select_computer_turn():
 
 
 def alpha_beta_trace_input():
-    active = input('\nDo you want to activate alpha-beta algorithm  ?\n 1. Yes  2.No \n')
+    active = input('\nDo you want to activate alpha-beta algorithm  ?\n 1. Yes  2. No \n')
     if len(active) > 1 or (active.isnumeric() and (int(active) > 2 or int(active) < 1)):
         print('Invalid Input! Choose 1 or 2')
         return alpha_beta_trace_input()
 
-    trace = input('\nDo you want to generate trace file ?\n 1. Yes  2.No \n')
+    trace = input('\nDo you want to generate trace file ?\n 1. Yes  2. No \n')
     if len(trace) > 1 or (trace.isnumeric() and (int(trace) > 2 or int(trace) < 1)):
         print('Invalid Input! Choose 1 or 2')
         return alpha_beta_trace_input()
@@ -62,20 +63,20 @@ def alpha_beta_trace_input():
     is_active = True if int(active) is 1 else False
     is_trace = True if int(trace) is 1 else False
 
-    return is_active,is_trace
+    return is_active, is_trace
 
 
 def assign_player_choices(play_mode='human'):
     choice = input('\nPlayer 1, what do you want to play with? (dots or color): \n')
     choice = choice.upper()
     players = [None for i in range(NUM_PLAYERS)]
-    players[0] = Player(choice,'Player 1')
-    players[1] = Player('COLOR' if choice == 'DOTS' else 'DOTS' , 'Player 2' if play_mode == 'human' else 'AI')
+    players[0] = Player(choice, 'Player 1')
+    players[1] = Player('COLOR' if choice == 'DOTS' else 'DOTS', 'Player 2' if play_mode == 'human' else 'AI')
     return players
 
 
 def assign_computer_choices():
-    random_number = randint(1,2)
+    random_number = randint(1, 2)
     choice = 'DOTS' if random_number is 1 else 'COLOR'
     print('\nAI has choosen to play with ' + choice)
     players = [None for i in range(NUM_PLAYERS)]
@@ -109,35 +110,45 @@ def perform_player_recycling_move(board):
         return perform_player_recycling_move(board)
     return True
 
-def generate_states_from_position(parent_state_node, position_moves):
+
+def is_valid_place_card_move(pick_card, place_card):
+    if (pick_card.first_cell.x == place_card.first_cell.x and pick_card.first_cell.y == place_card.first_cell.y
+            and pick_card.second_cell.x == place_card.second_cell.x and
+            pick_card.second_cell.y == place_card.second_cell.y):
+        if pick_card.rotation == place_card.rotation:
+            return False
+    return True
+
+
+def generate_states_from_position(parent_state_node, position_moves, pick_card=None):
     current_state = parent_state_node.get_data()
     move_state_nodes = []
     for move in position_moves:
-         x1 = move[0][0]
-         y1 = move[0][1]
-         x2 = move[1][0]
-         y2 = move[1][1]
-         for i in range(4):
+        x1 = move[0][0]
+        y1 = move[0][1]
+        x2 = move[1][0]
+        for i in range(4):
             card = Card(ROTATIONS[(2 * i) if x1 == x2 else (2 * i + 1)], y1, x1)
-            state = State(current_state, card)
-            state_node = StateNode(state)
-            state_node.parent = parent_state_node
-            move_state_nodes.append(state_node)
+            if pick_card is None or (pick_card is not None and is_valid_place_card_move(pick_card, card)):
+                state = State(current_state, card)
+                state_node = StateNode(state)
+                state_node.parent = parent_state_node
+                move_state_nodes.append(state_node)
     return move_state_nodes
 
 
-def get_children_states(current_state_node):
+def get_children_states(current_state_node, pick_card=None):
     current_state = current_state_node.get_data()
     available_positions = current_state.get_placeable_available_positions()
     move_state_nodes = []
     for position in available_positions:
         position_moves = current_state.generate_init_position_moves(position)
         # print(position_moves)
-        move_state_nodes = move_state_nodes + generate_states_from_position(current_state_node, position_moves)
+        move_state_nodes = move_state_nodes + generate_states_from_position(current_state_node, position_moves, pick_card)
     return move_state_nodes
 
 
-def perform_ai_regular_move(current_state, board,ai_choice,is_active,is_trace):
+def perform_ai_regular_move(current_state, board, ai_choice, is_active, is_trace):
     root_state_node = StateNode(current_state)
     root_state_node.children = get_children_states(root_state_node)
     leaf_nodes = []
@@ -148,15 +159,15 @@ def perform_ai_regular_move(current_state, board,ai_choice,is_active,is_trace):
     count = 0
     for leaf in leaf_nodes:
         leaf.heuristic_value = leaf.get_data().get_heuristic_value()
-        count = count  + 1
+        count = count + 1
 
     decision_state_node = None
     algo = None
     if is_active:
-        algo = AlphaBeta(root_state_node,ai_choice)
+        algo = AlphaBeta(root_state_node, ai_choice)
         decision_state_node = algo.alpha_beta_algorithm()
     else:
-        algo = MiniMax(root_state_node,ai_choice)
+        algo = MiniMax(root_state_node, ai_choice)
         decision_state_node = algo.minimax_algorithm()
 
     if is_trace:
@@ -168,7 +179,19 @@ def perform_ai_regular_move(current_state, board,ai_choice,is_active,is_trace):
     board.place_card(decision_state_node.data.card)
 
 
+def perform_ai_recycling_move(current_state, board):
+    card_positions = current_state.get_pickable_available_cards()
+    root_state_node = StateNode(current_state)
+    for position in card_positions:
+        pick_state = State(board)
+        pick_card = pick_state.get_card(ROTATIONS, position)
+        pick_state.remove_card(position, pick_state.cards[position])
+        state_node = StateNode(pick_state)
+        root_state_node.children = root_state_node.children + get_children_states(state_node, pick_card)
+    print(root_state_node.children)
+
 dt = np.dtype('U10')
+
 
 def print_board(board):
     print_matrix = np.empty((12, 8), dtype=dt)
@@ -179,19 +202,22 @@ def print_board(board):
             if board.matrix_data[i][j] is not None:
                 dot_type = board.matrix_data[i][j].get_dot_type()
                 color_type = board.matrix_data[i][j].get_color_type()
-                print_matrix[i][j] = ('RC' if color_type == COLOR[0] else 'WC') +\
-                                 ('*' if dot_type == DOT[0] else 'o')
+                print_matrix[i][j] = ('RC' if color_type == COLOR[0] else 'WC') + \
+                                     ('*' if dot_type == DOT[0] else 'o')
 
     # print(tabulate(np.flip(print_matrix, 0), headers, tablefmt="fancy_grid"))
-    flipped_matrix = np.flip(print_matrix,0)
-    print('\n'.join([''.join(['{:5}'.format(item if item is not '' else 'None') for item in row]) for row in flipped_matrix]))
+    flipped_matrix = np.flip(print_matrix, 0)
+    print('\n'.join(
+        [''.join(['{:5}'.format(item if item is not '' else 'None') for item in row]) for row in flipped_matrix]))
     print(''.join('{:5}'.format(index) for index in headers))
+
 
 n = -1
 
+
 def next_player():
     global n
-    n+=1
+    n += 1
     return n % NUM_PLAYERS
 
 
@@ -219,13 +245,14 @@ if playMode == 1:
         print_board(board)
 
     if board.is_winner_found is True:
-        print(str(board.get_current_player().get_player_name()) + " with play choice " + str(board.get_current_player().get_play_choice()) + " won the game ")
+        print(str(board.get_current_player().get_player_name()) + " with play choice " + str(
+            board.get_current_player().get_play_choice()) + " won the game ")
     else:
         print('Game draw!')
 
 if playMode == 2:
     print('\nYou have chosen to play with Computer!')
-    (is_active,is_trace) = alpha_beta_trace_input()
+    (is_active, is_trace) = alpha_beta_trace_input()
     turn = select_computer_turn()
     if turn is 1:
         players = assign_computer_choices()
@@ -248,3 +275,6 @@ if playMode == 2:
         else:
             perform_player_regular_move(board)
         print_board(board)
+
+    current_state = State(board)
+    perform_ai_recycling_move(current_state, board)
